45b4df051c1d076e427a669cf6049b244e37d331
ITsPorky <wc.walsh16@gmail.com>
2023-01-29 17:41:29 +1000

    Added components

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 function Article() {
-  const [data, setData] = useState();
-  const [date, setDate] = useState(Date);
+  const [data, setData] = useState({
+    title: "",
+    content: "",
+  });
 
   useEffect(() => {
     fetch(`http://localhost:3030/article/0`)
       .then((res) => res.json())
       .then((json) => {
-        setData(json.content);
-        const date = new Date(json.date).toDateString();
-        setDate(date);
+        setData({
+          title: json.title,
+          content: json.content,
+        });
       });
   });
 
   return (
-    <div className="Blog">
-      <h1>data.title</h1>
-      <div className="blog-details">
+    <div className="article">
+      <h1>{data.title}</h1>
+      <div className="article-details">
         <b>Author: OpenAI davinci003</b>
-        <span>Published: {date}</span>
       </div>
-      <p>{data}</p>
+      <p>{data.content}</p>
     </div>
   );
 }
